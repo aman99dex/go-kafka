ed13639839e3229053540382dc79efb230cbdaa3
Aman Gupta <kumarguptaaman896@gmail.com>
2025-03-08 01:57:58 +0530

    made the files

---
+++ producer/producer.go
@@ -0,0 +1,28 @@
+package main
+
+import(
+
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text" `
+}
+
+func main(){
+	app:=fiber.New()
+	api:=app.Group("/api/v1")
+	api.Post("/comments", createComment)
+	app.Listen(":3000")
+}
+func createComment(c *fiber.Ctx)error{
+	cmt:= new(Comment)
+	if err:=c.BodyParser(cmt); err!=nil{
+		log.Println(err)
+		c.Status(400).JSON(&fiber.Map{
+			"success":false,
+			"message": err,
+		})
+		return err
+}	
+
+}
